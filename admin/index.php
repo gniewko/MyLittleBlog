ac7f5fc95bd71db66deb675a7c019c2d8f9977ce
rafalkalinski <rafalkalinski1@gmail.com>
2011-05-12 14:19:49 +0200

    zamiana div na li #7

--- admin/index.php
+++ admin/index.php
@@ -23,14 +23,16 @@ require ROOT . '/lib/utils.php';
 <?php
 
 if (isset ($_SESSION['msg'])) {
+    echo "<ul>";
     if (isarray ($_SESSION['msg'])) {
         foreach ($_SESSION['msg'] as $value) {
-            echo '<div>'.$value.'</div>';
+            echo "<li>".$value."</li>\n";
         }
     }
     else {
-        echo '<div>'. $_SESSION['msg'] ."</div>\n";
+        echo "<li>". $_SESSION['msg'] ."</li>\n";
     }
+    echo "</ul>\n";
     unset ($_SESSION['msg']);
 }
 
