84108f221e7e32a396f09803f7d9f66f86ae97cf
Gniewko <zbigniew.chodacki@home.net.pl>
2011-05-12 14:41:24 +0200

    update: podpiecie css;
    Podpiecie /static/mlb.css

--- admin/index.php
+++ admin/index.php
@@ -13,6 +13,7 @@ require ROOT . '/lib/utils.php';
 ?><html>
     <head>
         <title>MyLittleBlog</title>
+        <link href="/static/mlb.css" media="screen" rel="stylesheet" type="text/css" />
     </head>
     <body>
         <ul>
