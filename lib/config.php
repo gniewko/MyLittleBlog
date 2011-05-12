20a56e16d59a26fab20f7b153633355c222dc76e
Lukasz Harasimowicz <developer@hardev.pl>
2011-05-12 13:52:13 +0200

    update: formatowanie;

--- lib/config.php
+++ lib/config.php
@@ -1,6 +1,6 @@
 <?php
 
-define ('TITLE_LENGTH', 0);
-define ('BODY_LENGTH', 0);
-define ('ACTION_SUBJECT_LENGTH', 3);
-define ('ACTION_CONTENT_LENGTH', 10);
+define ('TITLE_LENGTH',             0);
+define ('BODY_LENGTH',              0);
+define ('ACTION_SUBJECT_LENGTH',    3);
+define ('ACTION_CONTENT_LENGTH',    10);
